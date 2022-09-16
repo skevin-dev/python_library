9d117c680d58bdb5f2c2530ac795d5aad6363357
Kevin <shyakakevin1@gmail.com>
2022-09-16 19:15:38 +0200

    calculate and visualize a gaussian distribution

--- Scripts/gaussian.py
+++ Scripts/gaussian.py
@@ -106,4 +106,62 @@ class Gaussian():
         plt.xlabel("data")
         plt.ylabel("Frequency")
 
-    
+    def pdf(self, x):
+        """Probability density function calculator for the gaussian distribution.
+        
+        Args:
+            x (float): point for calculating the probability density function
+            
+        
+        Returns:
+            float: probability density function output
+        """
+        
+        return (1.0 / (self.stdev * math.sqrt(2*math.pi))) * math.exp(-0.5*((x - self.mean) / self.stdev) ** 2)
+        
+
+    def plot_histogram_pdf(self, n_spaces = 50):
+
+        """Function to plot the normalized histogram of the data and a plot of the 
+        probability density function along the same range
+        
+        Args:
+            n_spaces (int): number of data points 
+        
+        Returns:
+            list: x values for the pdf plot
+            list: y values for the pdf plot
+            
+        """
+        
+        mu = self.mean
+        sigma = self.stdev
+
+        min_range = min(self.data)
+        max_range = max(self.data)
+        
+         # calculates the interval between x values
+        interval = 1.0 * (max_range - min_range) / n_spaces
+
+        x = []
+        y = []
+        
+        # calculate the x values to visualize
+        for i in range(n_spaces):
+            tmp = min_range + interval*i
+            x.append(tmp)
+            y.append(self.pdf(tmp))
+
+        # make the plots
+        fig, axes = plt.subplots(2,sharex=True)
+        fig.subplots_adjust(hspace=.5)
+        axes[0].hist(self.data, density=True)
+        axes[0].set_title('Normed Histogram of Data')
+        axes[0].set_ylabel('Density')
+
+        axes[1].plot(x, y)
+        axes[1].set_title('Normal Distribution for \n Sample Mean and Sample Standard Deviation')
+        axes[0].set_ylabel('Density')
+        plt.show()
+
+        return x, y
