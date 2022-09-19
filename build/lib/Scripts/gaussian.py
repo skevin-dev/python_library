5b791edec412aa4b89013407dcfbf5b73ccaac7d
Kevin <shyakakevin1@gmail.com>
2022-09-19 11:49:26 +0200

    building a package

---
+++ build/lib/Scripts/gaussian.py
@@ -0,0 +1,214 @@
+import math
+from statistics import stdev 
+import matplotlib.pyplot as plt 
+import numpy as np
+from .App_log import logger
+
+class Gaussian():
+    """ Gaussian distribution class for calculating and 
+    visualizing a gaussian distribution
+
+    Attributes:
+        mean(float) representing the mean value of the distribution
+        stdev(float) representing the standard deviation of the distribution
+        data_list(list of floats) a list of floats extracted from the data file
+    """
+
+    def __init__(self,mu=0,sigma=1):
+        self.mean = mu
+        self.stdev = sigma
+        self.data = []
+        logger.info("initializing class attributes")
+        
+    def calculate_mean(self):
+        """The method to calculate the mean of the data set
+
+        Args:
+
+             None 
+
+        Returns:
+
+            float: the mean of the dataset 
+        
+        """
+
+        mean_dataset =  np.mean(self.data)
+        self.mean = mean_dataset
+        logger.info("calculate the mean")
+
+        return self.mean
+
+
+    def calculate_stdev(self,sample=True):
+        """The method to calculate the standard deviation of the data set
+
+        Args:
+           
+            sample(bool) whether the data represent a sample or population
+
+        Returns:
+             
+             float: the standard deviation of the dataset 
+        
+        """
+
+        if sample:
+            n = len(self.data) -1 
+
+        else:
+            n = len(self.data)
+
+        variance = sum((x-self.mean)**2 for x in self.data)
+
+        stdev_dataset = math.sqrt(variance/n)
+
+        self.stdev = stdev_dataset
+        logger.info("calculate the standard deviation")
+
+
+        return self.stdev
+
+    def read_data_file(self,filepath,sample=True):
+        """The method to read in data from txt file. the text file should have one number
+        per line. After reading in the file, the mean and standard deviation are calculated 
+
+        Args:
+
+            file_name(file): file containing data 
+
+        returns: 
+               
+               None 
+        
+        
+        """
+
+        with open(filepath) as file:
+            data = []
+            line = file.readline()
+            while line:
+                data.append(int(line))
+                line = file.readline()
+        file.close()
+
+        self.data = data
+        self.mean = self.calculate_mean()
+        self.stdev = self.calculate_stdev(sample)
+        logger.info("read the file")
+
+
+    def plot_histogram(self):
+        """The method to plot the hisogram of the data
+
+        Args:
+            None
+
+        Returns: 
+              None
+        
+        """
+        plt.hist(self.data)
+        plt.title("Distribution of data")
+        plt.xlabel("data")
+        plt.ylabel("Frequency")
+        logger.info("plot histogram")
+
+
+    def pdf(self, x):
+        """Probability density function calculator for the gaussian distribution.
+        
+        Args:
+            x (float): point for calculating the probability density function
+            
+        
+        Returns:
+            float: probability density function output
+        """
+        
+        return (1.0 / (self.stdev * math.sqrt(2*math.pi))) * math.exp(-0.5*((x - self.mean) / self.stdev) ** 2)
+        
+
+    def plot_histogram_pdf(self, n_spaces = 50):
+
+        """Function to plot the normalized histogram of the data and a plot of the 
+        probability density function along the same range
+        
+        Args:
+            n_spaces (int): number of data points 
+        
+        Returns:
+            list: x values for the pdf plot
+            list: y values for the pdf plot
+            
+        """
+        
+        mu = self.mean
+        sigma = self.stdev
+
+        min_range = min(self.data)
+        max_range = max(self.data)
+        
+         # calculates the interval between x values
+        interval = 1.0 * (max_range - min_range) / n_spaces
+
+        x = []
+        y = []
+        
+        # calculate the x values to visualize
+        for i in range(n_spaces):
+            tmp = min_range + interval*i
+            x.append(tmp)
+            y.append(self.pdf(tmp))
+
+        # make the plots
+        fig, axes = plt.subplots(2,sharex=True)
+        fig.subplots_adjust(hspace=.5)
+        axes[0].hist(self.data, density=True)
+        axes[0].set_title('Normed Histogram of Data')
+        axes[0].set_ylabel('Density')
+
+        axes[1].plot(x, y)
+        axes[1].set_title('Normal Distribution for \n Sample Mean and Sample Standard Deviation')
+        axes[0].set_ylabel('Density')
+        plt.show()
+
+        return x, y
+
+    def __add__(self, other):
+        
+            
+        
+        """Function to add together two Gaussian distributions
+
+        Args:
+            other (Gaussian): Gaussian instance
+
+        Returns:
+            Gaussian: Gaussian distribution
+
+        """
+
+        result = Gaussian()
+        result.mean = self.mean + other.mean
+        result.stdev = math.sqrt(self.stdev ** 2 + other.stdev ** 2)
+        logger.info("add to gaussian")
+
+
+        return result
+
+
+    def __repr__(self):
+        
+    
+        """Function to output the characteristics of the Gaussian instance
+        
+        Args:
+            None
+        
+        Returns:
+            string: characteristics of the Gaussian
+        
+        """
+        
+        return "mean {}, standard deviation {}".format(self.mean, self.stdev)
--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(name='distributions',
       version='0.1',
       description='Gaussian distributions',
-      packages=['distributions'],
+      packages=['Scripts'],
       zip_safe=False)
